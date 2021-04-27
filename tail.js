7673f5aa27cad3347b122f05972c0847749019c5
Astrid Aguilar <mixaguilar@yahoo.com>
2021-04-27 01:57:48 +0000

    Implemented tail function

---
+++ tail.js
@@ -0,0 +1,31 @@
+// -------- ASSERTEQUAL IMPLEMENTATION --------- //
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🙆 ✅ ASSERTION PASSED: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🙅 ❎ ASSERTION FAILED: ${actual} !== ${expected}`);
+  }
+
+};
+
+// ---------- FUNCTION IMPLEMENTATION ---------- //
+
+const tail = function(newArray) {
+
+  const index = newArray.index;
+
+  if (index > -1) {
+    newArray.splice(index, 1);
+  }
+
+  return newArray;
+
+};
+
+// ----------------- TEST CODE ----------------- //
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
