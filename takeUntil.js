675f24d4bd5e7cd0b511e62dd2e6378fb24d8992
Astrid Aguilar <mixaguilar@yahoo.com>
2021-04-29 23:38:15 +0000

    implemented takeUntil function that returns a slice of the array with elements taken from the beginning

---
+++ takeUntil.js
@@ -0,0 +1,55 @@
+// - EQARRAYS & ASSERTARRAYSEQUAL IMPLEMENTATION - //
+
+const eqArrays = function(firstArray, secondArray) {
+
+  if (firstArray === secondArray) {
+    return true;
+  } else if (firstArray.length !== secondArray.length) {
+    return false;
+  } else if (firstArray === null || secondArray === null) {
+    return false;
+  }
+
+  return true;
+
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+
+  if (eqArrays(firstArray, secondArray) === true) {
+    console.log(`🙆 ✅ ASSERTION PASSED: ${firstArray} === ${secondArray}`);
+  } else if (eqArrays(firstArray, secondArray) === false) {
+    console.log(`🙅 ❎ ASSERTION FAILED: ${firstArray} !== ${secondArray}`);
+  }
+
+};
+
+// ---------- FUNCTION IMPLEMENTATION ---------- //
+
+const takeUntil = function(array, callback) {
+
+  let newArray = [];
+
+  for (const element of array) {
+    if (!callback(element)) {
+      newArray.push(element);
+    } else {
+      return newArray;
+    }
+  }
+
+};
+
+// ----------------- TEST CODE ----------------- //
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
