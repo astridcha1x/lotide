70fe61577bc5a901d2a031c6485315c6b8f245b6
Astrid Aguilar <mixaguilar@yahoo.com>
2021-04-29 22:29:12 +0000

    implemented map function that returns a new array based on the results of the callback function

---
+++ map.js
@@ -0,0 +1,47 @@
+// - EQARRAYS & ASSERTARRAYSEQUAL IMPLEMENTATION - //
+
+const eqArrays = function(firstArray, secondArray) {
+
+  if (firstArray === secondArray) {
+    return true;
+  } else if (firstArray.length !== secondArray.length) {
+    return false;
+  } else if (firstArray === null || secondArray === null) {
+    return false;
+  }
+
+  return true;
+
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+
+  if (eqArrays(firstArray, secondArray) === true) {
+    console.log(`🙆 ✅ ASSERTION PASSED: ${firstArray} === ${secondArray}`);
+  } else if (eqArrays(firstArray, secondArray) === false) {
+    console.log(`🙅 ❎ ASSERTION FAILED: ${firstArray} !== ${secondArray}`);
+  }
+
+};
+
+// ---------- FUNCTION IMPLEMENTATION ---------- //
+
+const map = function(array, callback) {
+
+  const output = [];
+
+  for (const item of array) {
+    results.push(callback(item));
+  }
+
+  return output;
+
+};
+
+// ----------------- TEST CODE ----------------- //
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+console.log(assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]));
